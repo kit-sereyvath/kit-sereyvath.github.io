610317200dfbc0a2f5d179db6eb73a0d61877404
kimsereyvath <ksereyvath@outlook.com>
2022-02-16 06:47:25 +0700

    fix/ change some elements position.

--- src/app/components/search/search.component.ts
+++ src/app/components/search/search.component.ts
@@ -13,9 +13,9 @@ export class SearchComponent implements OnInit {
   label_time:string = 'Time:';
   label_province:string = 'Province:';
   
-  times: string[] = ['none', '1am', '2am', '3am', '4am', '5am', '6am', '7am', '8am', '9am', '10am', '11am', '12am', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm', '9pm', '10pm', '11pm', '12pm'];
+  times: string[] = ['1am', '2am', '3am', '4am', '5am', '6am', '7am', '8am', '9am', '10am', '11am', '12am', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm', '9pm', '10pm', '11pm', '12pm'];
 
-  locations: string[] = ['--- none ---', 'Banteay Meanchey', 'Battambang', 'Kampong Cham', 'Kampong Chhnang', 'Kampong Speu', 'Kampong Thom', 'Kampot', 'Koh Kong', 'Kratié', 'Mondulkiri', 'Phnom Penh', 'Preah Vihear', 'Prey Veng', ' Pursat', 'Ratanak Kiri', 'Siem Reap', 'Preah Sihanouk Ville', 'Stung Treng', 'Svay Rieng', 'Tbong Khmum', 'Takéo', 'Oddar Meanchey', 'Kep', 'Pailin'];
+  locations: string[] = ['Banteay Meanchey', 'Battambang', 'Kampong Cham', 'Kampong Chhnang', 'Kampong Speu', 'Kampong Thom', 'Kampot', 'Koh Kong', 'Kratié', 'Mondulkiri', 'Phnom Penh', 'Preah Vihear', 'Prey Veng', ' Pursat', 'Ratanak Kiri', 'Siem Reap', 'Preah Sihanouk Ville', 'Stung Treng', 'Svay Rieng', 'Tbong Khmum', 'Takéo', 'Oddar Meanchey', 'Kep', 'Pailin'];
 
   constructor(
     private router: Router,
